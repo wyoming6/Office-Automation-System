6a11b45363d66fc64027c5db84dd1cdd38e5e93b
wyoming6 <catherine.sun20@gmail.com>
2023-04-17 19:34:53 -0400

    integrate knife4j

---
+++ common/service-util/src/main/java/com/atguigu/common/config/knife4j/SwaggerBootstrapUiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.common.config.knife4j;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@SpringBootApplication
+public class SwaggerBootstrapUiDemoApplication extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,8 +3,8 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.service.SysRoleService;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@Api(tags="Role Management")
+@Api(tags="Role Management")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -29,7 +29,7 @@ public class SysRoleController {
 //    }
 
     //return all roles
-//    @ApiOperation(value = "All roles")
+    @ApiOperation(value = "All roles")
     @GetMapping("/findAll")
     public Result findAll(){
         List<SysRole> list = sysRoleService.list();
