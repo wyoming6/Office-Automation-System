7c6e129c97860895508a855b5182836fd08b9231
wyoming6 <catherine.sun20@gmail.com>
2023-04-18 21:25:34 -0400

    Page query

--- service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
@@ -2,14 +2,21 @@ package com.atguigu.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@MapperScan("com.atguigu.auth.mapper")
+@ComponentScan("com.atguigu") //默认搜索当前包及其子包的内容。这里可以搜索到所有com.atguigu路径下的component
 public class ServiceAuthApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ServiceAuthApplication.class, args);
+
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ServiceAuthApplication.class);
+//
+//        for (String beanName : context.getBeanDefinitionNames()) {
+//            System.out.println(beanName);
+//        }
     }
 }
--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,16 +3,16 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.service.SysRoleService;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import com.atguigu.vo.system.SysRoleQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(tags="Role Management")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -29,10 +29,103 @@ public class SysRoleController {
 //    }
 
     //return all roles
-    @ApiOperation(value = "All roles")
     @GetMapping("/findAll")
     public Result findAll(){
         List<SysRole> list = sysRoleService.list();
         return Result.ok(list);
     }
+
+
+    /**
+     * 带条件的分页查询
+     * @param page 当前页
+     * @param limit 每页记录数
+     * @param sysRoleQueryVo 条件对象
+     * @return
+     */
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo){
+        Page<SysRole> sysRolePage = new Page<>(page, limit);
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if(!StringUtils.isEmpty(roleName)){
+            wrapper.like(SysRole::getRoleName, roleName);
+        }
+        IPage<SysRole> page1 = sysRoleService.page(sysRolePage, wrapper);
+        return Result.ok(page1);
+    }
+
+    /**
+     * 添加角色
+     * @param role 角色
+     * @return
+     */
+    //以下这种方式也可以：
+    //public Result save(SysRole role)
+    //@GetMapping("save")
+    @PostMapping("save")
+    public Result save(@RequestBody SysRole role){
+        boolean isSaved = sysRoleService.save(role);
+        if(isSaved){
+            return Result.ok();
+        }
+        return Result.fail();
+    }
+
+    /**
+     * 查询角色
+     * @param id 角色id
+     * @return
+     */
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id){
+        SysRole byId = sysRoleService.getById(id);
+        return Result.ok(byId);
+    }
+
+    /**
+     * 修改角色（最终修改）
+     * @param role 角色
+     * @return
+     */
+    @PutMapping("update")
+    public Result update(@RequestBody SysRole role){
+        boolean isSuccessful = sysRoleService.updateById(role);
+        if(isSuccessful){
+            return Result.ok();
+        }
+        return Result.fail();
+    }
+
+    /**
+     * 删除角色
+     * @param id
+     * @return
+     */
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id){
+        boolean isSuccessful = sysRoleService.removeById(id);
+        if(isSuccessful){
+            return Result.ok();
+        }
+        return Result.fail();
+
+    }
+
+    /**
+     * 批量删除
+     * @param idList
+     * @return
+     */
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList){
+        boolean isSuccessful = sysRoleService.removeByIds(idList);
+        if(isSuccessful){
+            return Result.ok();
+        }
+        return Result.fail();
+    }
+
 }
