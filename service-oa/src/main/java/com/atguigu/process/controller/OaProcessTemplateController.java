d7003bc6b11cb7781d7f1bf6138ec15918a655f8
wyoming6 <catherine.sun20@gmail.com>
2023-05-24 19:23:32 -0400

    Approval settings - approval templates

--- service-oa/src/main/java/com/atguigu/process/controller/OaProcessTemplateController.java
+++ service-oa/src/main/java/com/atguigu/process/controller/OaProcessTemplateController.java
@@ -1,13 +1,144 @@
 package com.atguigu.process.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.common.result.Result;
+import com.atguigu.model.process.ProcessTemplate;
+import com.atguigu.process.service.OaProcessTemplateService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/admin/process/processTemplate")
 public class OaProcessTemplateController {
+    @Autowired
+    private OaProcessTemplateService processTemplateService;
 
+    /**
+     * 分页查询审批模板
+     * @param page
+     * @param limit
+     * @return
+     */
+    @GetMapping("{page}/{limit}")
+    public Result index(@PathVariable Long page,
+                        @PathVariable Long limit) {
+        Page<ProcessTemplate> pageParam = new Page<>(page,limit);
+        //根据oa_process_template表中的process_type_id字段，查询oa_process_type表中的name字段
+        IPage<ProcessTemplate> pageModel =
+                processTemplateService.selectPageProcessTempate(pageParam);
+        return Result.ok(pageModel);
+    }
+
+    /**
+     * 查询
+     * @param id
+     * @return
+     */
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.list')")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        ProcessTemplate processTemplate = processTemplateService.getById(id);
+        return Result.ok(processTemplate);
+    }
+
+    /**
+     * 保存
+     * @param processTemplate
+     * @return
+     */
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.templateSet')")
+    @PostMapping("save")
+    public Result save(@RequestBody ProcessTemplate processTemplate) {
+        processTemplateService.save(processTemplate);
+        return Result.ok();
+    }
+
+    /**
+     * 修改
+     * @param processTemplate
+     * @return
+     */
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.templateSet')")
+    @PutMapping("update")
+    public Result updateById(@RequestBody ProcessTemplate processTemplate) {
+        processTemplateService.updateById(processTemplate);
+        return Result.ok();
+    }
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.remove')")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        processTemplateService.removeById(id);
+        return Result.ok();
+    }
+
+    @PostMapping("/uploadProcessDefinition")
+    public Result uploadProcessDefinition(MultipartFile file) throws FileNotFoundException {
+        //获取classes目录位置
+        String path = new File(ResourceUtils.getURL("classpath:")
+                .getPath()).getAbsolutePath();
+
+        //设置上传文件夹
+        File tempFile = new File(path + "/processes/");
+        if(!tempFile.exists()) {
+            tempFile.mkdirs();
+        }
+        //创建空文件，实现文件写入
+        String filename = file.getOriginalFilename();
+        File zipFile = new File(path + "/processes/" + filename);
+
+        //保存文件
+        try {
+            file.transferTo(zipFile);
+        } catch (IOException e) {
+            return Result.fail();
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        //根据上传地址后续部署流程定义，设置文件名称为流程定义的key
+        map.put("processDefinitionPath", "processes/" + filename);
+        map.put("processDefinitionKey", filename.substring(0, filename.lastIndexOf(".")));
+        return Result.ok(map);
+    }
+
+    //部署流程定义（发布）
+    @GetMapping("/publish/{id}")
+    public Result publish(@PathVariable Long id) {
+        //流程定义部署
+        processTemplateService.publish(id);
+        return Result.ok();
+    }
+
+    public static void main(String[] args) {
+        try {
+            String path = new File(ResourceUtils.getURL("classpath:")
+                    .getPath()).getAbsolutePath();
+            System.out.println("getURL:");
+            System.out.println(ResourceUtils.getURL("classpath:"));
+            System.out.println("getPath:");
+            System.out.println(ResourceUtils.getURL("classpath:")
+                    .getPath());
+            System.out.println("AbsolutePath:");
+            System.out.println(path);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
--- service-oa/src/main/java/com/atguigu/process/service/impl/OaProcessTemplateServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/process/service/impl/OaProcessTemplateServiceImpl.java
@@ -15,16 +15,44 @@ import org.springframework.util.StringUtils;
 
 import java.util.List;
 
-/**
- * <p>
- * 审批模板 服务实现类
- * </p>
- *
- * @author atguigu
- * @since 2023-02-14
- */
+
 @Service
 public class OaProcessTemplateServiceImpl extends ServiceImpl<OaProcessTemplateMapper, ProcessTemplate> implements OaProcessTemplateService {
+    @Autowired
+    private OaProcessTypeService processTypeService;
 
+    @Override
+    public IPage<ProcessTemplate> selectPageProcessTempate(Page<ProcessTemplate> pageParam) {
+        //1 调用mapper的方法实现分页查询
+        Page<ProcessTemplate> processTemplatePage = baseMapper.selectPage(pageParam, null);
 
+        //2 第一步分页查询返回分页数据，从分页数据获取列表list集合
+        List<ProcessTemplate> processTemplateList = processTemplatePage.getRecords();
+
+        //3 遍历list集合，得到每个对象的审批类型id
+        for(ProcessTemplate processTemplate : processTemplateList) {
+            //得到每个对象的审批类型id
+            Long processTypeId = processTemplate.getProcessTypeId();
+            //4 根据审批类型id，查询获取对应名称
+            LambdaQueryWrapper<ProcessType> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(ProcessType::getId,processTypeId);
+            ProcessType processType = processTypeService.getOne(wrapper);
+            if(processType == null) {
+                continue;
+            }
+            //5 完成最终封装processTypeName
+            processTemplate.setProcessTypeName(processType.getName());
+        }
+        return processTemplatePage;
+    }
+
+    @Override
+    public void publish(Long id) {
+        //修改数据库表oa_process_template中的status字段。1表示发布。
+        ProcessTemplate processTemplate = baseMapper.selectById(id);
+        processTemplate.setStatus(1);
+        baseMapper.updateById(processTemplate);
+
+        //todo: to be continued
+    }
 }
